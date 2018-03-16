22d66ca44081a102a83475b13ca7c6faef9c7adb
Wes Dollar <wes@wesdollar.com>
2018-03-15 21:27:52 -0400

    adding ability to grab all from directory page then parse each individual page within directory for data

--- scrape.js
+++ scrape.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const headless = false
+const headless = true
 
 async function getCategories(page) {
 
@@ -39,6 +39,69 @@ async function getCategoryResultsCount(page, href) {
     })
 }
 
+async function getAllProductLinksOnPage(page, href) {
+
+    await page.goto(href)
+
+    return await page.evaluate(() => {
+
+        let els = document.querySelectorAll('.product_pod')
+        let data = []
+
+        for (let i = 0; i < els.length; i++) {
+
+            let el = els[i].querySelector('h3 a')
+            let title = el.innerText
+            let href = el.href
+
+            data.push({
+                title: title,
+                href: href,
+            })
+        }
+
+        return data
+    })
+}
+
+async function parseAllProductPagesInCategory(page, href) {
+
+    let pages = await getAllProductLinksOnPage(page, href)
+
+    let data = {
+        meta: {
+            results: pages.length
+        },
+        results: [],
+    }
+
+    for (let i = 0; i < pages.length; i++) {
+
+        let href = pages[i].href
+
+        data.results.push(await getBookInfoByPage(page, href))
+    }
+
+    return data
+}
+
+async function getBookInfoByPage(page, href) {
+
+    await page.goto(href)
+
+    return await page.evaluate(() => {
+
+        let title = document.querySelector('.product_main h1').innerText
+        let price = document.querySelector('.product_main .price_color').innerText
+
+        return {
+
+            title: title,
+            price: price,
+        }
+    })
+}
+
 async function getAllCategoriesWithResultsCount() {
 
     const browser = await puppeteer.launch({ headless: headless }) // {headless: false}
@@ -51,6 +114,7 @@ async function getAllCategoriesWithResultsCount() {
     for (let i = 0; i < categories.length; i++) {
 
         let count = await getCategoryResultsCount(page, categories[i].href)
+        // let books = await getCategoryBooks(page, categories[i].href)
 
         data.push({
 
@@ -64,7 +128,26 @@ async function getAllCategoriesWithResultsCount() {
     return data
 }
 
-getAllCategoriesWithResultsCount().then((value) => {
+// getBookInfoByPage().then((value) => {
+//
+//     console.log(value); // Success!
+// })
 
-    console.log(value); // Success!
+async function test() {
+
+    const browser = await puppeteer.launch({ headless: headless }) // {headless: false}
+    const page = await browser.newPage()
+
+    let href = 'http://books.toscrape.com/catalogue/category/books/food-and-drink_33/index.html'
+
+    let data = await parseAllProductPagesInCategory(page, href)
+
+    browser.close()
+
+    return data
+}
+
+test().then((value) => {
+
+    console.log(value)
 })
