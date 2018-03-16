9a59d73d5a51dc82ad388e6095c1f8fee01ef9b0
Wes Dollar <wes@wesdollar.com>
2018-03-15 21:53:33 -0400

    tieing everything together to get categories from home page and dive down to each product page within category to scrape info, no pagination at this point

--- scrape.js
+++ scrape.js
@@ -2,17 +2,15 @@ const puppeteer = require('puppeteer');
 
 const headless = true
 
-async function getCategories(page) {
+async function getCategories(page, href) {
 
-    await page.goto('http://books.toscrape.com/catalogue/category/books/travel_2/index.html')
+    await page.goto(href)
 
     return await page.evaluate(() => {
 
         let data = []
 
         let categories = document.querySelectorAll('.nav ul li')
-
-        // return categories
 
         for (let i = 0; i < categories.length; i++) {
 
@@ -26,16 +24,6 @@ async function getCategories(page) {
         }
 
         return data
-    })
-}
-
-async function getCategoryResultsCount(page, href) {
-
-    await page.goto(href)
-
-    return await page.evaluate(() => {
-
-        return document.querySelector('#default > div > div > div > div > form > strong').innerText
     })
 }
 
@@ -64,13 +52,14 @@ async function getAllProductLinksOnPage(page, href) {
     })
 }
 
-async function parseAllProductPagesInCategory(page, href) {
+async function parseAllProductPagesInCategory(page, href, categoryTitle) {
 
     let pages = await getAllProductLinksOnPage(page, href)
 
     let data = {
         meta: {
-            results: pages.length
+            title: categoryTitle,
+            results: pages.length,
         },
         results: [],
     }
@@ -102,52 +91,64 @@ async function getBookInfoByPage(page, href) {
     })
 }
 
-async function getAllCategoriesWithResultsCount() {
+// async function getCategoryResultsCount(page, href) {
+//
+//     await page.goto(href)
+//
+//     return await page.evaluate(() => {
+//
+//         return document.querySelector('#default > div > div > div > div > form > strong').innerText
+//     })
+// }
+
+// async function getAllCategoriesWithResultsCount() {
+//
+//     const browser = await puppeteer.launch({ headless: headless }) // {headless: false}
+//     const page = await browser.newPage()
+//
+//     let categories = await getCategories(page)
+//
+//     let data = []
+//
+//     for (let i = 0; i < categories.length; i++) {
+//
+//         let count = await getCategoryResultsCount(page, categories[i].href)
+//         // let books = await getCategoryBooks(page, categories[i].href)
+//
+//         data.push({
+//
+//             title: categories[i].title,
+//             count: count
+//         })
+//     }
+//
+//     browser.close()
+//
+//     return data
+// }
+
+async function run() {
 
     const browser = await puppeteer.launch({ headless: headless }) // {headless: false}
     const page = await browser.newPage()
 
-    let categories = await getCategories(page)
+    let entryHref = 'http://books.toscrape.com'
 
-    let data = []
+    let categories = await getCategories(page, entryHref)
+
+    let result = []
 
     for (let i = 0; i < categories.length; i++) {
 
-        let count = await getCategoryResultsCount(page, categories[i].href)
-        // let books = await getCategoryBooks(page, categories[i].href)
-
-        data.push({
-
-            title: categories[i].title,
-            count: count
-        })
+        result.push(await parseAllProductPagesInCategory(page, categories[i].href, categories[i].title))
     }
 
     browser.close()
 
-    return data
+    return result
 }
 
-// getBookInfoByPage().then((value) => {
-//
-//     console.log(value); // Success!
-// })
-
-async function test() {
-
-    const browser = await puppeteer.launch({ headless: headless }) // {headless: false}
-    const page = await browser.newPage()
-
-    let href = 'http://books.toscrape.com/catalogue/category/books/food-and-drink_33/index.html'
-
-    let data = await parseAllProductPagesInCategory(page, href)
-
-    browser.close()
-
-    return data
-}
-
-test().then((value) => {
+run().then((value) => {
 
     console.log(value)
 })
